cc9f5617624034e52be898ba87924b8b4516d0e6
Linu09 <38865727+Linu09@users.noreply.github.com>
2021-01-10 11:30:32 +0400

    Update main.py

--- main.py
+++ main.py
@@ -21,21 +21,21 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 *******************************************************************************
 ''')
 print("Welcome to Treasure Island.")
-print("Your mission is to find the treasure.") 
-choice1 = input("You are at a cross road which way you want to go? left or right \n").lower()
+print("Your mission is to find the treasure.")
+choice1 = input('You\'re at a cross road. Where do you want to go? Type "left" or "right" \n').lower()
 if choice1 == "left":
-  choice2 = input("Do you want to swim or wait? \n").lower()
+  choice2 = input('You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across. \n').lower()
   if choice2 == "wait":
-    choice3 = input("Which door you want to open ? \n Red , Blue or Yellow \n").lower()
-    if choice3 == "blue":
-      print("Eaten by Beast \n ...GAME OVER...")
-    elif choice3 == "red":
-      print(" Burned by Fire \n ...GAME OVER...")
+    choice3 = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose? \n").lower()
+    if choice3 == "red":
+      print("It's a room full of fire. Game Over.")
     elif choice3 == "yellow":
-      print("You WIN !!!!")
+      print("You found the treasure! You Win!")
+    elif choice3 == "blue":
+      print("You enter a room of beasts. Game Over.")
     else:
-      print("GAME OVER")
+      print("You chose a door that doesn't exist. Game Over.")
   else:
-    print("Attacked by a TROUT \n ...GAME OVER... ")
+    print("You get attacked by an angry trout. Game Over.")
 else:
-  print(" You Fall in to a hole \n ...GAME OVER...")
+  print("You fell into a hole. Game Over.")
